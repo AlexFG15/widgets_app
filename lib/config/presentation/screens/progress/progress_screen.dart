aac8baf01eaa336470949d6c0e59046ca4da33d8
Alex <alejandrofg15@hotmail.com>
2025-05-26 14:28:24 -0600

    Progress indicator finish

--- lib/config/presentation/screens/progress/progress_screen.dart
+++ lib/config/presentation/screens/progress/progress_screen.dart
@@ -6,6 +6,62 @@ class ProgressScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('Progress Indicators')),
+      body: _ProgreesView(),
+    );
+  }
+}
+
+class _ProgreesView extends StatelessWidget {
+  const _ProgreesView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          SizedBox(height: 30),
+          Text('Circula progress indicator'),
+
+          SizedBox(height: 10),
+          CircularProgressIndicator(
+            strokeWidth: 2,
+            backgroundColor: Colors.black45,
+          ),
+
+          SizedBox(height: 20),
+          Text('Circula y Linea controlado'),
+          _ControlledProgresIndicator(),
+        ],
+      ),
+    );
+  }
+}
+
+class _ControlledProgresIndicator extends StatelessWidget {
+  const _ControlledProgresIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Stream.periodic(Duration(milliseconds: 300), (value) {
+        return (value * 2) / 100;
+      }).takeWhile((value) => value < 100),
+      builder: (context, snapshot) {
+        final progressValue = snapshot.data;
+        return Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(value: progressValue, strokeWidth: 2),
+              SizedBox(width: 20),
+              Expanded(child: LinearProgressIndicator(value: progressValue)),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
